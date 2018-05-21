1843d05a29e61d0b4dd23cf2f693adb51ffb93ec
jvoisin <julien.voisin@dustri.org>
2018-05-21 22:52:42 +0200

    Give a first shot at writing a setup.py file

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mat2",
+    version='0.1.1',
+    author="Julien (jvoisin) Voisin",
+    author_email="julien.voisin+mat2@dustri.org",
+    description="A handy tool to trash your metadata",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://0xacab.org/jvoisin/mat2",
+    python_requires = '>=3.5.0',
+    scripts=['mat2.py'],
+    install_requires=[
+        'mutagen',
+        'PyGObject',
+        'pycairo',
+    ],
+    packages=setuptools.find_packages(exclude=('tests', )),
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Security",
+        "Intended Audience :: End Users/Desktop",
+    ),
+    project_urls={
+        'bugtacker': 'https://0xacab.org/jvoisin/mat2/issues',
+    },
+)
