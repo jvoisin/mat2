a3081bce47bc475a7afaa25e834e8afe399dc350
Rui Chen <rui@chenrui.dev>
2023-08-02 23:17:49 +0000

    setup: use share/man/man1 for man1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         'pycairo',
     ],
     packages=setuptools.find_packages(exclude=('tests', )),
-    data_files = [('man/man1', ['doc/mat2.1'])],
+    data_files = [('share/man/man1', ['doc/mat2.1'])],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
