jvoisin/mat2

commit f07344444d6d2f04a1f93e2954f4910b194bee0c

Fix a broken test
Reported-By: https://github.com/NixOS/nixpkgs/issues/436421

tests/test_libmat2.py
@@ -640,12 +640,8 @@ class TestCleaning(unittest.TestCase):
 
         with open('./tests/data/clean.html', 'w') as f:
             f.write('<title><title><pouet/><meta/></title></title><test/>')
-        p = web.HTMLParser('./tests/data/clean.html')
-        self.assertTrue(p.remove_all())
-        with open('./tests/data/clean.cleaned.html', 'r') as f:
-            self.assertEqual(f.read(), '<title></title><test/>')
-        os.remove('./tests/data/clean.html')
-        os.remove('./tests/data/clean.cleaned.html')
+        with self.assertRaises(ValueError):
+            p = web.HTMLParser('./tests/data/clean.html')
 
         with open('./tests/data/clean.html', 'w') as f:
             f.write('<test><title>Some<b>metadata</b><br/></title></test>')

commit 7a8ea224bc327b8ee929379d577c74968ea1c352

Fix issue introduced in f073444
The continuous integration on 0xacab didn't run, so it didn't catch this issue.
It seems like we'll have to move to github or whatever instead, sigh.

tests/test_libmat2.py
@@ -4,6 +4,7 @@ import unittest
 import shutil
 import os
 import re
+import sys
 import tarfile
 import tempfile
 import zipfile
@@ -638,10 +639,20 @@ class TestCleaning(unittest.TestCase):
         os.remove('./tests/data/clean.cleaned.html')
         os.remove('./tests/data/clean.cleaned.cleaned.html')
 
-        with open('./tests/data/clean.html', 'w') as f:
-            f.write('<title><title><pouet/><meta/></title></title><test/>')
-        with self.assertRaises(ValueError):
+        if sys.version_info >= (3, 13):
+            with open('./tests/data/clean.html', 'w') as f:
+                f.write('<title><title><pouet/><meta/></title></title><test/>')
+            with self.assertRaises(ValueError):
+                p = web.HTMLParser('./tests/data/clean.html')
+        else:
+            with open('./tests/data/clean.html', 'w') as f:
+                f.write('<title><title><pouet/><meta/></title></title><test/>')
             p = web.HTMLParser('./tests/data/clean.html')
+            self.assertTrue(p.remove_all())
+            with open('./tests/data/clean.cleaned.html', 'r') as f:
+                self.assertEqual(f.read(), '<title></title><test/>')
+            os.remove('./tests/data/clean.cleaned.html')
+        os.remove('./tests/data/clean.html')
 
         with open('./tests/data/clean.html', 'w') as f:
             f.write('<test><title>Some<b>metadata</b><br/></title></test>')